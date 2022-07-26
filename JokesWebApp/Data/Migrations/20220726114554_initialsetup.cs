82f5df9b59f9eff48290313c0bd1252ec1506342
Beckdotan <dotan.beck@gmail.com>
2022-07-26 16:33:28 +0300

    Finished app

---
+++ JokesWebApp/Data/Migrations/20220726114554_initialsetup.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+//how to add migrations in mac:
+//https://docs.microsoft.com/en-us/ef/core/cli/dotnet
+
+//running the code in the 
+
+
+namespace JokesWebApp.Data.Migrations
+{
+    public partial class initialsetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Joke",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    JokeQuestion = table.Column<string>(nullable: true),
+                    JokeAnswer = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Joke", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Joke");
+        }
+    }
+}
